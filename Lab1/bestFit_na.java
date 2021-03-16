98167384eea8fe89a02bc858d9b5b7c3191e0c6a
AinaAzerin <ainaazerin@gmail.com>
2021-03-16 13:04:29 +0800

    use ArrayList

--- Lab1/bestFit_na.java
+++ Lab1/bestFit_na.java
@@ -5,8 +5,8 @@ public class bestFit_na{
         System.out.println("First Fit Process:");
         int partition[]={200, 700, 300, 100, 600};
         int job[]={212, 417, 112, 426, 200};
-        int num_process=job.length;
-        int num_block=partition.length;
+        int num_process=job.length; //number of process
+        int num_block=partition.length; //slot of blocks
 
         bestfit(partition,job,num_process,num_block);
     }
@@ -14,22 +14,22 @@ public class bestFit_na{
     public static void bestfit(int partition[],int job[],int num_process,int num_block){
         ArrayList<Integer> block=new ArrayList<>();
         for(int i=0;i<num_process;i++){
-            int best=-1;
+            int best=-1; //do the checking start with 0
             for(int j=0;j<num_block;j++){
                 if(job[i]<=partition[j]) {
                     if (best == -1) {
                         best = j;
-                    } else if (partition[best] > partition[j]) {
+                    } else if (partition[best] > partition[j]) { //choose the smaller block size
                         best = j;
                     }
                 }
             }
             if(best != -1){
-                block.add(i,best);
-                partition[best] =-1;
+                block.add(i,best); //add at index i
+                partition[best] =-1; //remove block(cannot choose anymore)
             }
             else
-                block.add(i,-1);
+                block.add(i,-1); //index not allocated
         }
         System.out.println("\nProcess No.\tProcess Size\tBlock No.");
         for(int i=0;i<num_process;i++){
--- Lab1/firstFit_na.java
+++ Lab1/firstFit_na.java
@@ -5,8 +5,8 @@ public class firstFit_na {
         System.out.println("First Fit Process:");
         int partition[]={200, 700, 300, 100, 600};
         int job[]={212, 417, 112, 426, 200};
-        int num_process=job.length;
-        int num_block=partition.length;
+        int num_process=job.length; //num of process
+        int num_block=partition.length; //slot of blocks
 
         firstfit(partition,job,num_process,num_block);
     }
@@ -16,12 +16,12 @@ public class firstFit_na {
         for(int i=0;i<num_process;i++){
             for(int j=0;j<num_block;j++){
                 if(job[i]<=partition[j]){
-                    block.add(i,j);
-                    partition[j]=-1;
+                    block.add(i,j); //add at index i
+                    partition[j]=-1;//remove block(cannot choose anymore)
                     break;
                 }
                 else
-                    block.add(i,-1);
+                    block.add(i,-1);//index not allocated
             }
         }
         System.out.println("\nProcess No.\tProcess Size\tBlock No.");
