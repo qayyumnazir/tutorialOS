ec7f25420fa3ccbbe0d4e67822bdce934a99e746
AinaAzerin <ainaazerin@gmail.com>
2021-03-16 12:46:16 +0800

    use arraylist

---
+++ Lab1/firstFit_na.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class firstFit_na {
+    public static void main(String[] args){
+        System.out.println("First Fit Process:");
+        int partition[]={200, 700, 300, 100, 600};
+        int job[]={212, 417, 112, 426, 200};
+        int num_process=job.length;
+        int num_block=partition.length;
+
+        firstfit(partition,job,num_process,num_block);
+    }
+
+    public static void firstfit(int partition[],int job[],int num_process,int num_block){
+        ArrayList<Integer> block=new ArrayList<>();
+        for(int i=0;i<num_process;i++){
+            for(int j=0;j<num_block;j++){
+                if(job[i]<=partition[j]){
+                    block.add(i,j);
+                    partition[j]=-1;
+                    break;
+                }
+                else
+                    block.add(i,-1);
+            }
+        }
+        System.out.println("\nProcess No.\tProcess Size\tBlock No.");
+        for(int i=0;i<num_process;i++){
+            System.out.print(""+(i+1)+"\t\t\t "+job[i]+"\t\t\t ");
+            if (block.get(i)!=-1){
+                System.out.print(block.get(i)+1);
+            }
+            else {
+                System.out.print("It cannot be allocated");
+            }
+            System.out.println();
+        }
+    }
+}
