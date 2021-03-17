e0939e27a1ca7e6a929f22dff87df94349988c1f
Qayyum <qayyumblazer@gmail.com>
2021-03-17 15:00:14 +0800

    rename

---
+++ Lab2/fifo_yum.java
@@ -0,0 +1,81 @@
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+
+
+/**
+ * FIFO
+ */
+public class FIFO {
+
+    public static void main(String[] args) {
+        LinkedList<String> a = new LinkedList<>();
+        a.push("A");
+        a.push("B");
+        a.push("A");
+        a.push("C");
+        a.push("A");
+        a.push("B");
+        a.push("D");
+        a.push("B");
+        a.push("A");
+        a.push("C");
+        a.push("D");
+
+        fifosim(3, a);
+    }
+
+    public static void fifosim(int size,LinkedList<String> a)
+    {
+        //creating the page frame
+        String[] frame= new String[size];
+        for(int i=0;i<size;i++)
+        {
+            frame[i]="Empty";
+        }
+        int num=0;
+        int round=a.size();
+
+        for(int i=0;i<round;i+=1)
+        {
+            //check kalau dah ada kat frAME 
+            boolean masuk=false;
+            for(int j=0;j<frame.length;j++)
+            {
+                if(frame[j].equals(a.peekLast()))
+                {
+                    a.removeLast();
+                    masuk=true;
+                    break;
+                }
+                
+
+                //check kalu ada kosong
+                if (frame[j].equalsIgnoreCase("empty"))
+                {
+                    frame[j]=a.removeLast();
+                    masuk=true;
+                    break;
+                    
+                }
+            
+
+            }
+            //algo if takde
+            if(!masuk)
+            {
+                    if(num>=size)num=0;
+                    frame[num]=a.removeLast();
+                    num++;
+            }
+
+
+            for(int l=0;l<size;l++)
+            System.out.println(l+1+" is "+frame[l]);
+            System.out.println("------------------");
+        }
+
+
+    }
+}
